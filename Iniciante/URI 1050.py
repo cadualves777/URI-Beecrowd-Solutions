2cf316230317e65b8e6fc9edba7ed9b8c2c1b18b
cadualves777 <83433476+cadualves777@users.noreply.github.com>
2023-07-31 09:45:07 -0300

    Create URI 1050.py

---
+++ Iniciante/URI 1050.py
@@ -0,0 +1,19 @@
+ddd = int(input())
+uf = 'DDD nao cadastrado'
+if ddd == 11:
+    uf = 'Sao Paulo'
+elif ddd == 19:
+    uf = 'Campinas'
+elif ddd == 21:
+    uf = 'Rio de Janeiro'
+elif ddd == 27:
+    uf = 'Vitoria'
+elif ddd == 31:
+    uf = 'Belo Horizonte'
+elif ddd == 32:
+    uf = 'Juiz de Fora'
+elif ddd == 61:
+    uf = 'Brasilia'
+elif ddd == 71:
+    uf = 'Salvador'
+print(uf)
